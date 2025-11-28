68f5b9975c3429f5300ef1b8d15f811dd76d3661
Malith <malith@Manupriya-PC.>
2025-11-29 04:43:19 +0530

    Working Gazebo

---
+++ src/rarm/bring_up/rsp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Single script to launch the myCobot with Gazebo and ROS 2 Controllers
+ 
+cleanup() {
+    echo "Cleaning up..."
+    sleep 5.0
+    pkill -9 -f "ros2|gazebo|gz|nav2|amcl|bt_navigator|nav_to_pose|rviz2|assisted_teleop|cmd_vel_relay|robot_state_publisher|joint_state_publisher|move_to_free|mqtt|autodock|cliff_detection|moveit|move_group|basic_navigator"
+}
+ 
+# Set up cleanup trap
+trap 'cleanup' SIGINT SIGTERM
+ . ~/rarm_ws/install/setup.bash
+echo "Launching Gazebo simulation..."
+ros2 launch rarm robot_state_publisher.launch
